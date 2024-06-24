293b81754b9fa496aee0c439d3b522b40151efff
thejmitchener <thejmitchener@users.noreply.github.com>
2024-06-24 16:53:58 +0000

    Fix styling

--- src/Commands/LaravelInitCommand.php
+++ src/Commands/LaravelInitCommand.php
@@ -53,7 +53,7 @@ class LaravelInitCommand extends Command
         $this->runShellCommand("php artisan forms:install {$force}");
         $this->runShellCommand('php artisan deploy:install || true');
 
-        $this->runShellCommand("php artisan storage:link");
+        $this->runShellCommand('php artisan storage:link');
         $this->runShellCommand("php artisan migrate {$force}");
 
         $this->info('Packages installed successfully.');
