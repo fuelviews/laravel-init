f41efef26c120681a80deb5e546ac3968e3f4308
thejmitchener <joshua@fuelviews.com>
2024-11-14 14:35:56 -0500

    Refactor AppInitServiceProvider to include new commands.

--- src/AppInitServiceProvider.php
+++ src/AppInitServiceProvider.php
@@ -29,5 +29,7 @@ class AppInitServiceProvider extends PackageServiceProvider
                 InstallComposerPackagesCommand::class,
                 ConfigureEnvCommand::class,
             ]);
+
     }
+
 }
